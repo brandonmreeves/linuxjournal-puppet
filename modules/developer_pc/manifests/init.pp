46d0e4806fe107feb687f5977569e82122c454b5
David Barton <dave@oneit.com.au>
2015-08-30 17:50:46 +0800

    Creating Static Files - Listing 5

--- modules/developer_pc/manifests/init.pp
+++ modules/developer_pc/manifests/init.pp
@@ -6,6 +6,13 @@ class developer_pc ($developer) {
         shell => '/bin/bash',
         managehome => true,
     }    
+
+    file { "/home/$developer/.vimrc":
+        source => "puppet:///modules/developer_pc/vimrc",
+        owner => "$developer",
+        group => "$developer",
+        require => [ User["$developer"] ]
+    }
 }
 
 
